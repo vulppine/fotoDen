37d26d9068c6e2238ee25a954ea7753ea6b88eb2
vulppine <vulppine@gmail.com>
2021-02-15 00:20:18 -0800

    Actually adds in the code for building, whoops

---
+++ tool/build.go
@@ -0,0 +1,121 @@
+package tool
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+// BuildFile represents a fotoDen build file.
+type BuildFile struct {
+	Name     string   `yaml:"name"`
+	Dir      string   `yaml:"dir"`
+	Desc     string   `yaml:"desc"`
+	Type     string   `yaml:"type"`
+	Thumb    string   `yaml:"thumb"`
+	Static   bool     `yaml:"static"`
+	ImageDir string   `yaml:"imageDir"`
+	Images   []string `yaml:"images,flow"`
+	Options  struct {
+		Copy     bool `yaml:"copy"`
+		Sort     bool `yaml:"sort"`
+		Meta     bool `yaml:"metadata"`
+		Gensizes bool `yaml:"generateSizes"`
+	} `yaml:"imageOptions,flow"`
+	Subfolders []*BuildFile `yaml:"subfolders,flow"`
+}
+
+// OpenYAML opens a build file into a new BuildFile object.
+func (b *BuildFile) OpenBuildYAML(file string) error {
+	f, err := ioutil.ReadFile(file)
+	if checkError(err) {
+		return err
+	}
+
+	err = yaml.Unmarshal(f, &b)
+	if checkError(err) {
+		return err
+	}
+
+	return nil
+}
+
+// BuildFromYAML is the entry point to the fotoDen
+// website build system. It takes a YAML file,
+// with the correct structure, and creates a website
+// in the given folder.
+func (b *BuildFile) Build(folder string) error {
+	verbose(fmt.Sprint(b))
+	switch b.Type {
+	case "folder":
+		genopts := GeneratorOptions{
+			Static: b.Static,
+		}
+		err := GenerateFolder(
+			FolderMeta{
+				Name: b.Name,
+				Desc: b.Desc,
+			},
+			folder,
+			genopts,
+		)
+		if checkError(err) {
+			return err
+		}
+
+		for _, f := range b.Subfolders {
+			if f.Dir == "" {
+				f.Dir = f.Name
+			}
+			err = f.Build(filepath.Join(folder, f.Dir))
+			if checkError(err) {
+				return err
+			}
+		}
+
+		return nil
+	case "album":
+		genopts := GeneratorOptions{
+			ImageGen: true,
+			Copy:     b.Options.Copy,
+			Sort:     b.Options.Sort,
+			Meta:     b.Options.Meta,
+			Static:   b.Static,
+			Gensizes: b.Options.Gensizes,
+		}
+		if b.ImageDir != "" {
+			genopts.Source = b.ImageDir
+			err := GenerateFolder(
+				FolderMeta{
+					Name: b.Name,
+					Desc: b.Desc,
+				},
+				folder,
+				genopts,
+			)
+			if checkError(err) {
+				return err
+			}
+
+			InsertImage(folder, "append", Genoptions, b.Images...)
+		} else {
+			err := GenerateFolder(
+				FolderMeta{
+					Name: b.Name,
+					Desc: b.Desc,
+				},
+				folder,
+				genopts,
+			)
+			if checkError(err) {
+				return err
+			}
+
+			InsertImage(folder, "append", Genoptions, b.Images...)
+		}
+	}
+
+	return nil
+}
---
+++ tool/cmd/build.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/vulppine/fotoDen/tool"
+)
+
+func init() {
+	rootCmd.AddCommand(buildCmd)
+}
+
+var (
+	buildCmd = &cobra.Command{
+		Use: "build buildfile destination",
+		Short: "Generates a fotoDen folder/album from a valid YAML file",
+		Args: cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			b := new(tool.BuildFile)
+			err := b.OpenBuildYAML(args[0])
+			if err != nil {
+				return err
+			}
+
+			err = b.Build(args[1])
+			if err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+)
